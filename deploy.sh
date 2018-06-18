92ddef7d3bdd304b31d06b9bd9ee316601c8ecf7
dogbeide <boyowao@gmail.com>
2018-06-17 22:22:04 -0400

    Implement automated deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+
+if [ ! -d $(pwd)/node_modules ]; then
+    yarn install
+fi
+
 yarn build && cd build
+
 git init
 git remote add origin git@github.com:dogbeide/dogbeide.github.io.git
-git pull
+git fetch origin
+git checkout master
+
 echo "# dogbeide.github.io" >> README.md
+
 git add README.md
 git add -A
 git commit -m "first commit"
+
 git push -u origin master
