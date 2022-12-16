351a8970930d87b5af99a759bf52249fa626c38f
David Ogbeide <boyowao@gmail.com>
2022-12-15 23:12:04 -0700

    compactify and upgrade responsive design for projects large and mobile screen

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -6,30 +6,27 @@ import TechIcon from './TechIcon';
 const Project = ({ url, img, alt, title, desc, techs }) => {
     
     return (
-        <div>
-            <div className="project">
-                <a
-                    href={url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <img
-                        className="project__img"
-                        src={img}
-                        alt={alt}
-                    />
-                </a>
-                <div className="project__info">
-                    <div className="project__title">{title}</div>
-                    <div className="project__desc">{desc}</div>
-                    <div className="project__techs">
-                    {techs ? techs.map((tech, i) =>
-                        <TechIcon key={i} tech={tech} />
-                    ) : null}
-                    </div>
+        <div className="project">
+            <a
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                <img
+                    className="project__img"
+                    src={img}
+                    alt={alt}
+                />
+            </a>
+            <div className="project__info">
+                <div className="project__title">{title}</div>
+                <div className="project__desc">{desc}</div>
+                <div className="project__techs">
+                {techs ? techs.map((tech, i) =>
+                    <TechIcon key={i} tech={tech} />
+                ) : null}
                 </div>
             </div>
-            <hr className="project__hr" />
         </div>
     )
 }
