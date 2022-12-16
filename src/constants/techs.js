078f9360261102c0ea60477051155c48bd2ea61d
David Ogbeide <boyowao@gmail.com>
2022-12-15 18:59:02 -0700

    fix techicons render error

--- src/constants/techs.js
+++ src/constants/techs.js
@@ -1,50 +1,37 @@
-
-export const techConstantNames = {
-    REACT: 'React',
-    REDUX: 'Redux',
-    NODE: 'Node',
-    FIREBASE: 'Firebase',
-    JAVASCRIPT: 'JavaScript',
-    HTML5: 'HTML5',
-    CSS3: 'CSS3',
-    PYTHON: 'Python',
-    DJANGO: 'Django'
-};
-
 export default {
-    REACT: {
+    'React': {
         id: 1,
         url: 'images/icons/react.png'
     },
-    REDUX: {
+    'Redux': {
         id: 2,
         url: 'images/icons/redux.svg'
     },
-    NODE: {
+    'Node': {
         id: 3,
         url: 'images/icons/node.svg'
     },
-    FIREBASE: {
+    'Firebase': {
         id: 4,
         url: 'images/icons/firebase.png'
     },
-    JAVASCRIPT: {
+    'JavaScript': {
         id: 5,
         url: 'images/icons/js.svg'
     },
-    HTML5: {
+    'HTML5': {
         id: 6,
         url: 'images/icons/html5.svg'
     },
-    CSS3: {
+    'CSS3': {
         id: 7,
         url: 'images/icons/css3.svg'
     },
-    PYTHON: {
+    'Python': {
         id: 8,
         url: 'images/icons/python.png'
     },
-    DJANGO: {
+    'Django': {
         id: 9,
         url: 'images/icons/django.svg'
     },
