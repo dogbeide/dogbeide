29be843303aa709f23036125a74dc8d27e121ee1
David Ogbeide <boyowao@gmail.com>
2022-12-15 23:38:57 -0700

    update deploy script for vite from create-react-app

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 if [ ! -d $(pwd)/node_modules ]; then
-  yarn install
+  npm install
 fi
 
 if [ ! -d $(pwd)/dogbeide.github.io ]; then
   git clone git@github.com:dogbeide/dogbeide.github.io.git
 fi
 
-yarn build
+npm run build
 cd dogbeide.github.io
-cp -r ../build/* .
+cp -r ../dist/* .
 
 git add -A
 git commit -m "deploy live build"
